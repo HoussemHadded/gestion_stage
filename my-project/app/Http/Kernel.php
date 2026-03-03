4766ac11660b99039e35dc7f48aeb623ef92284c
HoussemHadded <houssem.hadded@sesame.com.tn>
2026-03-03 04:01:53 +0000

    Sécurisation et Middleware

---
+++ my-project/app/Http/Kernel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    // Middlewares globaux
+    protected $middleware = [
+        // Exemple :
+        // \App\Http\Middleware\TrustProxies::class,
+        // \Fruitcake\Cors\HandleCors::class,
+        // \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        // \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        // \App\Http\Middleware\TrimStrings::class,
+        // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    // Groupes de middleware
+    protected $middlewareGroups = [
+        'web' => [
+            // Middleware typiques pour web
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    // Middlewares pour routes spécifiques
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'role' => \App\Http\Middleware\RoleMiddleware::class, // ton middleware
+        
+    ];
+}
--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -5,31 +5,43 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\OffreController;
 use App\Http\Controllers\CandidatureController;
 
-// Home
+// Home (publique)
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Users
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+// -------------------------
+// USERS (admin seulement)
+// -------------------------
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+});
 
-// Offres
-Route::get('/offres', [OffreController::class, 'index'])->name('offres.index');
-Route::get('/offres/create', [OffreController::class, 'create'])->name('offres.create');
-Route::post('/offres', [OffreController::class, 'store'])->name('offres.store');
-Route::get('/offres/{id}/edit', [OffreController::class, 'edit'])->name('offres.edit');
-Route::put('/offres/{id}', [OffreController::class, 'update'])->name('offres.update');
-Route::delete('/offres/{id}', [OffreController::class, 'destroy'])->name('offres.destroy');
+// -------------------------
+// OFFRES (admin + entreprise)
+// -------------------------
+Route::middleware(['auth:sanctum', 'role:admin,entreprise'])->group(function () {
+    Route::get('/offres', [OffreController::class, 'index'])->name('offres.index');
+    Route::get('/offres/create', [OffreController::class, 'create'])->name('offres.create');
+    Route::post('/offres', [OffreController::class, 'store'])->name('offres.store');
+    Route::get('/offres/{id}/edit', [OffreController::class, 'edit'])->name('offres.edit');
+    Route::put('/offres/{id}', [OffreController::class, 'update'])->name('offres.update');
+    Route::delete('/offres/{id}', [OffreController::class, 'destroy'])->name('offres.destroy');
+});
 
-// Candidatures
-Route::get('/candidatures', [CandidatureController::class, 'index'])->name('candidatures.index');
-Route::get('/candidatures/create', [CandidatureController::class, 'create'])->name('candidatures.create');
-Route::post('/candidatures', [CandidatureController::class, 'store'])->name('candidatures.store');
-Route::get('/candidatures/{id}/edit', [CandidatureController::class, 'edit'])->name('candidatures.edit');
-Route::put('/candidatures/{id}', [CandidatureController::class, 'update'])->name('candidatures.update');
-Route::delete('/candidatures/{id}', [CandidatureController::class, 'destroy'])->name('candidatures.destroy');
+// -------------------------
+// CANDIDATURES (admin + entreprise)
+// -------------------------
+Route::middleware(['auth:sanctum', 'role:admin,entreprise'])->group(function () {
+    Route::get('/candidatures', [CandidatureController::class, 'index'])->name('candidatures.index');
+    Route::get('/candidatures/create', [CandidatureController::class, 'create'])->name('candidatures.create');
+    Route::post('/candidatures', [CandidatureController::class, 'store'])->name('candidatures.store');
+    Route::get('/candidatures/{id}/edit', [CandidatureController::class, 'edit'])->name('candidatures.edit');
+    Route::put('/candidatures/{id}', [CandidatureController::class, 'update'])->name('candidatures.update');
+    Route::delete('/candidatures/{id}', [CandidatureController::class, 'destroy'])->name('candidatures.destroy');
+});
